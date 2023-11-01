62c9b1d6769a2cfc1bfdf31c687e02d243158093
David Chanin <chanindav@gmail.com>
2023-11-01 16:22:10 +0000

    output mean / stdev for each interations result

--- multitoken_estimator/training/BenchmarkIterationsResult.py
+++ multitoken_estimator/training/BenchmarkIterationsResult.py
@@ -2,13 +2,40 @@ from __future__ import annotations
 
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Callable, Mapping
+from statistics import stdev
+from typing import Any, Callable, Mapping, Sequence
 
 from multitoken_estimator.lib.util import mean, mean_values
 from multitoken_estimator.training.BenchmarkResult import (
     BenchmarkResult,
     CausalityTokenCountMethod,
 )
+
+
+@dataclass
+class AggregateMetric:
+    values: list[float]
+
+    @property
+    def mean(self) -> float:
+        return mean(self.values)
+
+    @property
+    def stdev(self) -> float:
+        return stdev(self.values)
+
+    def __repr__(self) -> str:
+        return f"{self.mean:.2f} ± {self.stdev:.2f}"
+
+
+def _agg_values(items: Sequence[Mapping[str, float]]) -> dict[str, AggregateMetric]:
+    """
+    return a dict of AggregateMetrics from lists of dicts of numbers
+    """
+    return {
+        key: AggregateMetric([item[key] for item in items if key in item])
+        for key in set(key for item in items for key in item.keys())
+    }
 
 
 @dataclass
@@ -26,13 +53,17 @@ class BenchmarkIterationsResult:
     def causality(self) -> float:
         return mean([result.causality for result in self.iteration_results])
 
-    def causality_avg_over_relations(self, min_total_per_relation: int = 0) -> float:
+    def causality_avg_over_relations(
+        self, min_total_per_relation: int = 0
+    ) -> AggregateMetric:
         # calculate causality by first finding causality per relation, and averaging relation scores
         # This means it doesn't matter if a relations has 1 test examples or 1000, they get the same weight
         causality_by_relation = self._filter_metric_by_total(
             self.causality_by_relation, min_total_per_relation
         )
-        return mean([causality for causality in causality_by_relation.values()])
+        return AggregateMetric(
+            [causality.mean for causality in causality_by_relation.values()]
+        )
 
     @property
     def causality_by_category(self) -> dict[str, float]:
@@ -42,105 +73,113 @@ class BenchmarkIterationsResult:
 
     def causality_by_category_avg_over_relations(
         self, min_total_per_relation: int = 0
-    ) -> dict[str, float]:
+    ) -> dict[str, AggregateMetric]:
         # calculate causality by first finding causality per relation, and averaging relation scores
         # This means it doesn't matter if a relations has 1 test examples or 1000, they get the same weight
         causality_by_relation = self._filter_metric_by_total(
             self.causality_by_relation, min_total_per_relation
         )
-        scores_by_category: dict[str, list[float]] = defaultdict(list)
+        scores_by_category: dict[str, list[AggregateMetric]] = defaultdict(list)
         for relation, causality in causality_by_relation.items():
             if relation in self.relation_category_map:
                 category = self.relation_category_map[relation]
                 scores_by_category[category].append(causality)
         return {
-            category: mean(scores) for category, scores in scores_by_category.items()
+            category: AggregateMetric([score.mean for score in scores])
+            for category, scores in scores_by_category.items()
         }
 
     @property
-    def causality_by_relation(self) -> dict[str, float]:
-        return mean_values(
+    def causality_by_relation(self) -> dict[str, AggregateMetric]:
+        return _agg_values(
             [result.causality_by_relation for result in self.iteration_results]
         )
 
     @property
-    def accuracy(self) -> float:
-        return mean([result.accuracy for result in self.iteration_results])
+    def accuracy(self) -> AggregateMetric:
+        return AggregateMetric([result.accuracy for result in self.iteration_results])
 
-    def accuracy_avg_over_relations(self, min_total_per_relation: int = 0) -> float:
+    def accuracy_avg_over_relations(
+        self, min_total_per_relation: int = 0
+    ) -> AggregateMetric:
         # calculate accuracy by first finding accuracy per relation, and averaging relation scores
         # This means it doesn't matter if a relations has 1 test examples or 1000, they get the same weight
         accuracy_by_relation = self._filter_metric_by_total(
             self.accuracy_by_relation, min_total_per_relation
         )
-        return mean([accuracy for accuracy in accuracy_by_relation.values()])
+        return AggregateMetric(
+            [accuracy.mean for accuracy in accuracy_by_relation.values()]
+        )
 
     @property
-    def accuracy_by_category(self) -> dict[str, float]:
-        return mean_values(
+    def accuracy_by_category(self) -> dict[str, AggregateMetric]:
+        return _agg_values(
             [result.accuracy_by_category for result in self.iteration_results]
         )
 
     def accuracy_by_category_avg_over_relations(
         self, min_total_per_relation: int = 0
-    ) -> dict[str, float]:
+    ) -> dict[str, AggregateMetric]:
         # calculate accuracy by first finding accuracy per relation, and averaging relation scores
         # This means it doesn't matter if a relations has 1 test examples or 1000, they get the same weight
         accuracy_by_relation = self._filter_metric_by_total(
             self.accuracy_by_relation, min_total_per_relation
         )
-        scores_by_category: dict[str, list[float]] = defaultdict(list)
+        scores_by_category: dict[str, list[AggregateMetric]] = defaultdict(list)
         for relation, accuracy in accuracy_by_relation.items():
             if relation in self.relation_category_map:
                 category = self.relation_category_map[relation]
                 scores_by_category[category].append(accuracy)
         return {
-            category: mean(scores) for category, scores in scores_by_category.items()
+            category: AggregateMetric([score.mean for score in scores])
+            for category, scores in scores_by_category.items()
         }
 
     def _filter_metric_by_total(
-        self, metric_per_relation: Mapping[str, float], min_total_per_relation: int = 0
-    ) -> dict[str, float]:
+        self,
+        metric_per_relation: Mapping[str, AggregateMetric],
+        min_total_per_relation: int = 0,
+    ) -> dict[str, AggregateMetric]:
         total_by_relation = self.total_by_relation
         return {
             relation: metric
             for relation, metric in metric_per_relation.items()
-            if total_by_relation[relation] >= min_total_per_relation
+            if total_by_relation[relation].mean >= min_total_per_relation
         }
 
     @property
-    def accuracy_by_relation(self) -> dict[str, float]:
-        return self._mean_vals(lambda res: res.accuracy_by_relation)
+    def accuracy_by_relation(self) -> dict[str, AggregateMetric]:
+        return self._agg_vals(lambda res: res.accuracy_by_relation)
 
     @property
-    def total_by_relation(self) -> dict[str, float]:
+    def total_by_relation(self) -> dict[str, AggregateMetric]:
         """Total averaged over all iterations"""
-        return self._mean_vals(lambda res: res.total_by_relation)
+        return self._agg_vals(lambda res: res.total_by_relation)
 
     @property
-    def accuracy_by_num_answer_tokens(self) -> dict[int, float]:
-        return self._mean_vals(lambda res: res.accuracy_by_num_answer_tokens)
+    def accuracy_by_num_answer_tokens(self) -> dict[int, AggregateMetric]:
+        return self._agg_vals(lambda res: res.accuracy_by_num_answer_tokens)
 
     def causality_by_num_answer_tokens(
         self,
         method: CausalityTokenCountMethod = "max_original_target",
-    ) -> dict[int, float]:
-        return self._mean_vals(lambda res: res.causality_by_num_answer_tokens(method))
+    ) -> dict[int, AggregateMetric]:
+        return self._agg_vals(lambda res: res.causality_by_num_answer_tokens(method))
 
     @property
-    def total_by_num_accuracy_answer_tokens(self) -> dict[int, float]:
+    def total_by_num_accuracy_answer_tokens(self) -> dict[int, AggregateMetric]:
         """Total averaged over all iterations"""
-        return self._mean_vals(lambda res: res.total_by_num_accuracy_answer_tokens)
+        return self._agg_vals(lambda res: res.total_by_num_accuracy_answer_tokens)
 
     def total_by_num_causality_answer_tokens(
         self, method: CausalityTokenCountMethod = "max_original_target"
-    ) -> dict[int, float]:
+    ) -> dict[int, AggregateMetric]:
         """Total averaged over all iterations"""
-        return self._mean_vals(
+        return self._agg_vals(
             lambda res: res.total_by_num_causality_answer_tokens(method)
         )
 
-    def _mean_vals(
+    def _agg_vals(
         self, result_cb: Callable[[BenchmarkResult], Mapping[Any, float]]
-    ) -> dict[Any, float]:
-        return mean_values([result_cb(result) for result in self.iteration_results])
+    ) -> dict[Any, AggregateMetric]:
+        return _agg_values([result_cb(result) for result in self.iteration_results])
