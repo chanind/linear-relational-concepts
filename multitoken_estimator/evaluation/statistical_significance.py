adc4395f9fa9b08a47f2662a225a55526f80471a
David Chanin <chanindav@gmail.com>
2023-11-12 12:11:23 +0000

    adding statistical significance test

---
+++ multitoken_estimator/evaluation/statistical_significance.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+import numpy as np
+import scipy.stats as stats
+
+
+@dataclass
+class StatisticalSignificanceResult:
+    p_value: float
+    z_score: float
+
+    def is_significant(self, significance_threshold=0.05) -> bool:
+        return self.p_value < significance_threshold
+
+    @property
+    def variation_a_wins(self) -> bool:
+        return self.z_score < 0
+
+    @property
+    def variation_b_wins(self) -> bool:
+        return self.z_score > 0
+
+
+def calculate_statistical_significance(
+    trials_a: int,
+    successes_a: int,
+    trials_b: int,
+    successes_b: int,
+) -> StatisticalSignificanceResult:
+    """
+    Calculate statistical significance between two trials, using 2-sided z-test
+    """
+    p1 = successes_a / trials_a
+    p2 = successes_b / trials_b
+
+    # Pooled proportion
+    p_pool = (successes_a + successes_b) / (trials_a + trials_b)
+
+    # Standard error
+    se = np.sqrt(p_pool * (1 - p_pool) * (1 / trials_a + 1 / trials_b))
+
+    # Z-score
+    z_score = (p2 - p1) / se
+
+    # P-value
+    p_value = 2 * stats.norm.cdf(-np.abs(z_score))
+
+    return StatisticalSignificanceResult(p_value=p_value, z_score=z_score)
---
+++ tests/evaluation/test_statistical_significance.py
@@ -0,0 +1,23 @@
+import pytest
+
+from multitoken_estimator.evaluation.statistical_significance import (
+    calculate_statistical_significance,
+)
+
+
+def test_statistical_significance() -> None:
+    # test that the results match https://abtestguide.com/calc/
+
+    result = calculate_statistical_significance(
+        trials_a=80000,
+        successes_a=1600,
+        trials_b=78000,
+        successes_b=1696,
+    )
+
+    assert result.p_value == pytest.approx(0.0154, abs=0.0001)
+    assert result.z_score == pytest.approx(2.4233, abs=0.01)
+    assert result.is_significant(0.05)
+    assert not result.is_significant(0.01)
+    assert result.variation_b_wins
+    assert not result.variation_a_wins
